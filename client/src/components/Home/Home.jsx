29bdc0e745e14bb3e2a5b9046044941a1cf433a1
Priyanshu Sharma <priyanshu.sharma.mzn@gmail.com>
2024-09-14 21:14:31 +0530

    background and color added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Home.module.css';
+import Card from '../Card/Card';
 
 function Home() {
     const texts = ["Marketing", "AWS", "Career", ""];
@@ -15,33 +16,44 @@ function Home() {
         return () => clearInterval(interval);
     }, []);
 
+    const handleClick = () => {
+        alert('Card clicked!');
+    };
+
     return (
         <>
-            <div className={styles.main}>
-                <div className={styles.uptext}>
-                    Want to learn a new skill ??
+            <div className={styles.bgdiv}>
+                <div className={styles.main}>
+                    <div className={styles.uptext}>
+                        Want to learn a new skill ??
+                    </div>
+                    <div className={styles.textContainer}>
+                        <div id={styles.beforePart}>Get 1-on-1  &nbsp;</div>
+                        <div id={styles.changingPart}>{texts[currentTextIndex]}</div>
+                        <div id={styles.afterPart}>
+                            Mentorship
+                        </div>
+                    </div>
+                    <div className="searchBar"></div>
                 </div>
-                <div className={styles.textContainer}>
-                    <div id={styles.beforePart}>Get 1-on-1  &nbsp;</div>
-                    <div id={styles.changingPart}>{texts[currentTextIndex]}</div>
-                    <div id={styles.afterPart}>
-                        Mentorship
+
+                <div className={styles.parentContainer}>
+                    <div className={styles.scrollingContent}>
+                        <Card
+                            name="John Doe"
+                            description="A passionate software developer."
+                            image="https://via.placeholder.com/300x150"
+                            onClick={handleClick}
+                        />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+
                     </div>
                 </div>
-                <div className="searchBar"></div>
             </div>
-            
-            <div className={styles.parentContainer}>
-                <div className={styles.scrollingContent}>
-                    <div className={styles.card}>Card 1</div>
-                    <div className={styles.card}>Card 2</div>
-                    <div className={styles.card}>Card 3</div>
-                    <div className={styles.card}>Card 4</div>
-                    <div className={styles.card}>Card 5</div>
-                    <div className={styles.card}>Card 6</div>
-                </div>
-            </div>
-            
+
         </>
     );
 }
