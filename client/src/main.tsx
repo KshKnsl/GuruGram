004f745167741b7ae9686b6d84a166d6c720584e
KushKansal <mrkushkansal@gmail.com>
2025-01-25 14:28:51 +0530

    SEO added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const clientId = import.meta.env.VITE_client_id|| "";
 import { Analytics } from "@vercel/analytics/react"
 
-
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Analytics/>
