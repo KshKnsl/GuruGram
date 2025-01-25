94bcdb7b6b653239cdb36df8e0bcecb202ab2b31
Kush Kansal <mrkushkansal@gmail.com>
2025-01-25 14:16:07 +0530

    Chat section made mobile responsive

--- client/src/Pages/chat/ChatPage.tsx
+++ client/src/Pages/chat/ChatPage.tsx
@@ -8,8 +8,8 @@ const ChatPage = () => {
   const { selectedUser } = useChatStore();
   return (
     <div className="min-h-screen bg-zinc-400 dark:bg-zinc-700">
-      <div className="flex items-center justify-center pt-20 px-4 ">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+      <div className="flex items-center justify-center md:pt-20 md:px-4 ">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl md:h-[calc(100vh-8rem)] h-screen">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
 
